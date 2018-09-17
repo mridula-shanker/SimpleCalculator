ca3d320855fd60eda96e25313acb47a7dd658786
mshanker22 <mshanker22@HS-MATH2-06.gvsd.org>
2018-09-17 13:37:51 -0400

    Learn addition

---
+++ Calculator Example/src/CalculatorMain.java
@@ -0,0 +1,10 @@
+
+public class CalculatorMain 
+{
+	public static void main(String[] args)
+	{
+			//This is how we create an object
+			Calculator calc = new Calculator();
+			System.out.println(calc.add(2, 6));
+	}
+}
